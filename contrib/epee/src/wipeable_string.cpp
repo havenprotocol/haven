d515ee9533f483e48e3bbd5d3be60af744da6e52
havenprotocol <havencurrency@gmail.com>
2018-03-30 21:16:00 +1300

    memwipe updates: call directly + check empty

--- contrib/epee/src/wipeable_string.cpp
+++ contrib/epee/src/wipeable_string.cpp
@@ -1,21 +1,21 @@
 // Copyright (c) 2017, The Monero Project
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -27,13 +27,12 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <string.h>
+#include "memwipe.h"
 #include "misc_log_ex.h"
 #include "wipeable_string.h"
 
 namespace epee
 {
-
-void *(*wipeable_string::wipefunc)(void*, size_t) = NULL;
 
 wipeable_string::wipeable_string(const wipeable_string &other):
   buffer(other.buffer)
@@ -55,12 +54,11 @@ wipeable_string::wipeable_string(const std::string &other)
 
 wipeable_string::wipeable_string(std::string &&other)
 {
-  CHECK_AND_ASSERT_THROW_MES(wipefunc, "wipefunc is not set");
   grow(other.size());
   memcpy(buffer.data(), other.c_str(), size());
   if (!other.empty())
   {
-    wipefunc(&other[0], other.size()); // we're kinda left with this again aren't we
+    memwipe(&other[0], other.size()); // we're kinda left with this again aren't we
     other = std::string();
   }
 }
@@ -78,30 +76,31 @@ wipeable_string::~wipeable_string()
 
 void wipeable_string::wipe()
 {
-  CHECK_AND_ASSERT_THROW_MES(wipefunc, "wipefunc is not set");
-  wipefunc(buffer.data(), buffer.size() * sizeof(char));
+  if (!buffer.empty())
+    memwipe(buffer.data(), buffer.size() * sizeof(char));
 }
 
 void wipeable_string::grow(size_t sz, size_t reserved)
 {
-  CHECK_AND_ASSERT_THROW_MES(wipefunc, "wipefunc is not set");
   if (reserved < sz)
     reserved = sz;
   if (reserved <= buffer.capacity())
   {
     if (sz < buffer.size())
-      wipefunc(buffer.data() + sz, buffer.size() - sz);
+      memwipe(buffer.data() + sz, buffer.size() - sz);
     buffer.resize(sz);
     return;
   }
   size_t old_sz = buffer.size();
   std::unique_ptr<char[]> tmp{new char[old_sz]};
   memcpy(tmp.get(), buffer.data(), old_sz * sizeof(char));
-  wipefunc(buffer.data(), old_sz * sizeof(char));
+  if (old_sz > 0)
+    memwipe(buffer.data(), old_sz * sizeof(char));
   buffer.reserve(reserved);
   buffer.resize(sz);
   memcpy(buffer.data(), tmp.get(), old_sz * sizeof(char));
-  wipefunc(tmp.get(), old_sz * sizeof(char));
+  if (old_sz > 0)
+    memwipe(tmp.get(), old_sz * sizeof(char));
 }
 
 void wipeable_string::push_back(char c)
