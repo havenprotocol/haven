7e6c8d1305ab7e4762acdc2510f4f29e954ceb83
donjor <donjordev@gmail.com>
2018-05-14 20:32:44 +1200

    Fix broken interactive daemon limit commands plus RPC calls from upstream

--- src/daemon/command_parser_executor.cpp
+++ src/daemon/command_parser_executor.cpp
@@ -367,8 +367,6 @@ bool t_command_parser_executor::set_limit(const std::vector<std::string>& args)
       std::cout << "failed to parse argument" << std::endl;
       return false;
   }
-  if (limit > 0)
-    limit *= 1024;
 
   return m_executor.set_limit(limit, limit);
 }
@@ -387,8 +385,6 @@ bool t_command_parser_executor::set_limit_up(const std::vector<std::string>& arg
       std::cout << "failed to parse argument" << std::endl;
       return false;
   }
-  if (limit > 0)
-    limit *= 1024;
 
   return m_executor.set_limit(0, limit);
 }
@@ -407,8 +403,6 @@ bool t_command_parser_executor::set_limit_down(const std::vector<std::string>& a
       std::cout << "failed to parse argument" << std::endl;
       return false;
   }
-  if (limit > 0)
-    limit *= 1024;
 
   return m_executor.set_limit(limit, 0);
 }
